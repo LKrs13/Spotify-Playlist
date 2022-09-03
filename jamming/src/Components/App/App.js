5442ee8cf69594b47246dba97428cf6873c3f489
LKrs13 <rayanesahi2002@gmail.com>
2022-09-02 23:41:19 -0400

    remove track & add name

--- jamming/src/Components/App/App.js
+++ jamming/src/Components/App/App.js
@@ -13,12 +13,21 @@ class App extends React.Component {
     this.state.playListName = 'New Playlist';
     this.state.playListTracks = [];
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.playListName = this.playListName.bind(this);
   }
   addTrack(track) {
     if (this.state.playListTracks.includes(track.id)) {
       return;
     }
     this.state.playListTracks.push(track);
+  }
+  removeTrack(track) {
+    const updatedPlaylist = this.state.playListTracks.filter(!track.id);
+    this.setState({playListTracks: updatedPlaylist});
+  }
+  updatedPlaylistName(name) {
+    this.setState({playListName: name});
   }
   render () {
     return (
@@ -28,7 +37,7 @@ class App extends React.Component {
         <SearchBar searchResults={this.state.searchResults}/>
         <div className="App-playlist">
           <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
-          <Playlist playListName={this.state.playListName} playListTracks={this.state.playListTracks}/>
+          <Playlist playListName={this.state.playListName} playListTracks={this.state.playListTracks} onNameChange={this.updatedPlaylistName}/>
         </div>
       </div>
     </div>
--- jamming/src/Components/Playlist/Playlist.js
+++ jamming/src/Components/Playlist/Playlist.js
@@ -3,11 +3,19 @@ import TrackList from "./TrackList";
 import PlaylistCSS from "./Playlist.css";
 
 export class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
   render() {
     return (
         <div className="Playlist">
             <input value={"New Playlist"}/>
-            <TrackList tracks={this.props.playListTracks}/>
+            <TrackList tracks={this.props.playListTracks} onRemove={this.props.removeTrack}/>
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     );
